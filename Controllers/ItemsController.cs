4f5881024470bab5dab37e4e3fa64be05ac65ce3
Ian Gregg <iangregg188@gmail.com>
2020-08-03 10:38:04 -0700

    added updated functionality with the edit in the items controller.
    Co-authored-by: Tyson Lackey <lackeyt90@gmail.com>

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -1,36 +1,47 @@
-// using Microsoft.AspNetCore.Mvc;
-// using Microsoft.AspNetCore.Mvc.Rendering;
-// using ToDoList.Models;
-// using System.Collections.Generic;
-// using System.Linq;
-// using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ToDoList.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
-// namespace ToDoList.Controllers
-// {
-//   public class ItemsController : Controller
-//   {
-//     private readonly ToDoListContext _db;
-//     public ItemsController(ToDoListContext db)
-//     {
-//       _db = db;
-//     }
-//     public ActionResult Details( int id )
-//     {
-//       Item thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
-//       return View(thisItem);
-//     }
-//     public ActionResult Create()
-//     {
-//     ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
-//     return View();
-//     }
-//     public ActionResult Edit (int id)
-//     {
-//       var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
-//       ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
-//       return View(thisItem);
-//     }
-    
+namespace ToDoList.Controllers
+{
+  public class ItemsController : Controller
+  {
+    private readonly ToDoListContext _db;
+    public ItemsController(ToDoListContext db)
+    {
+      _db = db;
+    }
+    public ActionResult Details(int id)
+    {
+      var thisItem = _db.Items
+          .Include(item => item.Categories)
+          .ThenInclude(join => join.Category)
+          .FirstOrDefault(item => item.ItemId == id);
+      return View(thisItem);
+    }
+    public ActionResult Create()
+    {
+    ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
+    return View();
+    }
+    public ActionResult Edit(int id)
+    {
+    var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+    ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
+    return View(thisItem);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Item item)
+    {
+        _db.Entry(item).State = EntityState.Modified;
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
+
 //     public ActionResult Delete(int id)
 //     {
 //       var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
@@ -45,25 +56,23 @@
 //       _db.SaveChanges();
 //       return RedirectToAction("Index");
 //     }
-//     [HttpPost]
-//     public ActionResult Edit(Item item)
-//     {
-//       _db.Entry(item).State = EntityState.Modified;
-//       _db.SaveChanges();
-//       return RedirectToAction("Index");
-//     }
-//     [HttpPost]
-//     public ActionResult Create(Item item)
-//     {
-//       _db.Items.Add(item);
-//       _db.SaveChanges();
-//       return RedirectToAction("Index");
-//     }
-//     public ActionResult Index()
-//     {
-//       List<Item> model = _db.Items.ToList();
-//       return View(model);
-//     }
-//   }
-// }
+
+    [HttpPost]
+    public ActionResult Create(Item item, int CategoryId)
+    {
+      _db.Items.Add(item);
+    if (CategoryId != 0)
+    {
+        _db.CategoryItem.Add(new CategoryItem() { CategoryId = CategoryId, ItemId = item.ItemId });
+    }
+    _db.SaveChanges();
+    return RedirectToAction("Index");
+    }
+    public ActionResult Index()
+    {
+      List<Item> model = _db.Items.ToList();
+      return View(model);
+    }
+  }
+}
 
